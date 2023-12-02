fa77930f333561659760675f16c824b5a886a255
fabiodemo <faberdemo@gmail.com>
2023-12-02 16:51:23 -0300

    Updated train for d4pg

--- train-d4pg.py
+++ train-d4pg.py
@@ -1,16 +1,24 @@
 import rclpy
 from turtle_env.turtle_env import Trainer
-from td3.td3_torch import Agent
+from d4pg.d4pg_torch import Agent
 
 
 def main(args=None):
     rclpy.init(args=args)
     trainer = Trainer()
-    agent = Agent(alpha=0.001, beta=0.001,
-            input_dims=trainer.env.num_states, tau=0.005,
-            max_action=trainer.env.action_upper_bound, min_action=trainer.env.action_lower_bound,
-            batch_size=64, layer1_size=150, layer2_size=256, # 400, 300
-            n_actions=trainer.env.num_actions)
+    agent = Agent(alpha=0.001,
+                  input_dims=trainer.env.num_states,
+                  tau=0.005,
+                  n_atoms=51,
+                  v_min=-10,
+                  v_max=10,
+                  max_action=trainer.env.action_upper_bound,
+                  min_action=trainer.env.action_lower_bound,
+                  batch_size=64,
+                  layer1_size=150,
+                  layer2_size=256,
+                  n_actions=trainer.env.num_actions,
+                  )
     trainer.train(agent, 5000, 140, False, 1)
     trainer.kill_env()
     rclpy.shutdown()
