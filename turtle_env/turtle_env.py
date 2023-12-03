9fcf1ec3b93bee343bc3eb1ea1ed9044194dcf7c
fabiodemo <faberdemo@gmail.com>
2023-12-03 20:07:33 -0300

    Correction in tensorboard writer

--- turtle_env/turtle_env.py
+++ turtle_env/turtle_env.py
@@ -316,8 +316,9 @@ class Trainer():
                 agent.save_models()
 
             if episode % 50 == 0:
+                # Writes to tensorboard
                 self.writer.add_scalar('Acumulated Reward each 50 episodes', acum_reward, episode)
-                self.writer.add_scalar('Moving Average Reward each 50 episodes', acum_reward, mov_avg_rwds)
+                self.writer.add_scalar('Moving Average Reward each 50 episodes', mov_avg_rwds[-1], episode)
                 # Plot raw rewards and moving average
                 plt.plot(acum_rwds, alpha=0.5, label="Raw Reward" if episode == 0 else "")
                 plt.plot(mov_avg_rwds, color='red', label="Moving Avg Reward" if episode == 0 else "")
