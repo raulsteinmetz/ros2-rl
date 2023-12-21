e3ba7c8cb29f733f56b8f47c38b641c22e06bfc7
Raaulsthub <rsteinmetz@inf.ufsm.br>
2023-12-21 08:54:54 -0300

    .

--- turtle_env/turtle_env.py
+++ turtle_env/turtle_env.py
@@ -185,13 +185,16 @@ class Env(Node):
                 self.target_x = random.uniform(1.60, 1.90)
                 self.target_y = random.uniform(1.60, 1.90)
         elif stage == 3:
-            area = np.random.randint(0, 2)
+            area = np.random.randint(0, 3)
             if area == 0:
-                self.target_x = random.uniform(-1.6, -1.9)
+                self.target_x = random.uniform(-1.8, -1.9)
                 self.target_y = random.uniform(-1.9, 1.9)
             elif area == 1:
                 self.target_x = random.uniform(-1.9, 1.9)
-                self.target_y = random.uniform(-1.6, 1.9)
+                self.target_y = random.uniform(-1.8, -1.9)
+            elif area == 2:
+                self.target_x = random.uniform(1.9, 1.1)
+                self.target_y = random.uniform(0.4, 1.1)
         elif stage == 4:
             area = np.random.randint(0, 3)
             if area == 0:
