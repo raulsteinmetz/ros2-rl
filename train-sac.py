c2d5f7eb4d41f7599b68e1eaa62c17e611a4b7aa
Raaulsthub <rsteinmetz@inf.ufsm.br>
2024-01-09 18:22:58 -0300

    cleaning todo, some other stuff

--- train-sac.py
+++ train-sac.py
@@ -5,7 +5,7 @@ from networks.sac.sac_torch import Agent
 
 def main(args=None):
     rclpy.init(args=args)
-    trainer = Trainer(algorithm_name='sac', stage=3)
+    trainer = Trainer(algorithm_name='sac', stage=4)
     agent = Agent(input_dims=[trainer.env.num_states], action_space_high=trainer.env.action_upper_bound, n_actions=trainer.env.num_actions)
     trainer.train(agent, 5000, 250, True)
     trainer.kill_env()
