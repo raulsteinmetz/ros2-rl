f8c64456a69aa311d4f728fefc4521c95d47690b
fabiodemo <faberdemo@gmail.com>
2024-03-09 11:27:36 -0300

    Trained DDPG - stage 1

--- train-ddpg.py
+++ train-ddpg.py
@@ -17,8 +17,8 @@ def main(args=None):
     num_actions = num_actions
 
     # stage - 1 & 2: batch_size=256, max_size=1000000
-    agent = Agent(alpha, beta, tau, input_dims=trainer.env.num_states, batch_size=256, fc1_dims=400, fc2_dims=300, n_actions=num_actions, max_size=2000000)
-    scores = trainer.train(agent, 5000, 160, False)
+    agent = Agent(alpha, beta, tau, input_dims=trainer.env.num_states, batch_size=512, fc1_dims=400, fc2_dims=300, n_actions=num_actions, max_size=2000000)
+    scores = trainer.train(agent, 5000, 500, True)
     trainer.kill_env()
     rclpy.shutdown()
 
